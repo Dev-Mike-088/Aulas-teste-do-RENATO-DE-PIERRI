f35dcbc1ad62171e0621d847991b1813796b2ff4
Mikael <mikaeleal28@gmail.com>
2026-08-11 21:42:38 -0300

    Segunda pergunta do Questionário

---
+++ pergunta2/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mikael Machado Souza");
+        System.out.println("Desenvolvimento de software Multiplataforma");
+        System.out.println("Curso de Java");
+    }
+}
