af3bdf65a429eaef34e4922143e5cc7e38aa68ce
Mikael <mikaeleal28@gmail.com>
2026-08-11 22:00:41 -0300

    Subindo a Pergunta 3

---
+++ pergunta3/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite o seu curso:");
+        String curso = sc.nextLine();
+
+        System.out.println("Digita a sua turma: ");
+        String turma = sc.nextLine();
+
+        System.out.println("=======Ficha do Aluno=======");
+        System.out.println("Nome: " + nome);
+        System.out.println("Curso: " + curso);
+        System.out.println("Turma: " + turma);
+    }
+}
