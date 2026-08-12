e9a83c3190725a109f0f3d8294608fd4c767cc31
Mikael <mikaeleal28@gmail.com>
2026-08-11 21:04:30 -0300

    Commit inicial

---
+++ pergunta1/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mikael Machado Souza");
+        System.out.println("Desenvolvimento de software Multiplataforma");
+    }
+    
+}
