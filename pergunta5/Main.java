46d47c7ba15392ee92c6c17567709f54007d51e0
Mikael <mikaeleal28@gmail.com>
2026-08-13 23:54:10 -0300

    Subindo pergunta 5

---
+++ pergunta5/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String situacao;
+        String nome = "Mikael Machado Souza";
+        int idade = 18;
+        double altura = 1.78;
+        double nota = 9.5;
+        boolean situacaoMatricula = true; // true para matriculado, false para não matriculado
+
+        if (situacaoMatricula){
+            situacao = "Matriculado";
+        } else {
+            situacao = "Não matriculado";
+        }
+
+        System.out.println("dados do aluno:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura + " metros");
+        System.out.println("Nota: " + nota);
+        System.out.println("Situação de matrícula: " + situacao);
+    }
+}
+//Declare variáveis para representar nome, idade, altura, nota e situação de matrícula de um aluno. Mostre todos os valores.
