dac9ce05723497b627eac66dec9275f421407d4b
Mikael <mikaeleal28@gmail.com>
2026-08-13 23:58:48 -0300

    Subindo pergunta 6

---
+++ pergunta6/Main.java
@@ -0,0 +1,9 @@
+//Armazene um nome e uma idade e produza uma frase no formato: “Marina possui 19 anos.”
+public class Main {
+    public static void main(String[] args){
+        String nome = "Mikael";
+        int idade = 20;
+
+        System.out.println(nome + " possui " + idade + " anos.");
+    }
+}
