e3a2ccd669dc8ae6c32ad275926e45286c7afc45
Mikael <mikaeleal28@gmail.com>
2026-08-15 20:38:25 -0300

    Subindo pergunta 25

--- pergunta24/Main.java
+++ pergunta24/Main.java
@@ -1,7 +1,38 @@
+//Lâmpada: crie uma classe com cor e estado ligada/desligada. Implemente ligar(), desligar() e mostrarEstado().
 
+import java.util.Scanner;
 
 public class Main{
     public static void main(){
+        Scanner sc = new Scanner(System.in);
+        Lampada lampada = new Lampada();
+        int opcao = 0;
+
+        do{
+            System.out.println("O que deseja fazer?");
+            System.out.println("1 - Ligar a lâmpada");
+            System.out.println("2 - Desligar a lâmpada");
+            System.out.println("3 - Mostrar estado da lâmpada");
+            System.out.println("0 - Sair");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    lampada.ligar();
+                    break;
+                case 2:
+                    lampada.desligar();
+                    break;
+                case 3:
+                    lampada.mostrarEstado();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }while(opcao != 0);    
 
     }
 }
