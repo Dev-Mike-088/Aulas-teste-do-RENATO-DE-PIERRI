e5d6e6d122d171563d41653ad2174715aa794701
Mikael <mikaeleal28@gmail.com>
2026-08-15 16:52:38 -0300

    Subindo pergunta 9 e 19

---
+++ pergunta19/Main.java
@@ -0,0 +1,42 @@
+//Crie uma calculadora interativa que leia dois números e apresente as quatro operações aritméticas.
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Escolha o que deseja fazer com os números:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Resto da divisão\nDigite o número da opção desejada: ");
+        int opcao = sc.nextInt();
+        System.out.print("Digite o primeiro número: ");
+        int num1 = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int num2 = sc.nextInt(); 
+
+        switch(opcao){
+            case 1:
+                System.out.println("O resultado da soma é: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("O resultado da subtração é: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("O resultado da multiplicação é: " + (num1 * num2));
+                break;
+            case 4:
+                if(num2 != 0){
+                    System.out.println("O resultado da divisão é: " + (num1 / num2));
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                }
+                break;
+            case 5:
+                if(num2 != 0){
+                    System.out.println("O resultado do resto dessa divisão é: " + (num1 / num2));
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                }    
+                break;
+            default:
+                System.out.println("Opção inválida.");            
+        }
+    }
+}
--- pergunta9/Main.java
+++ pergunta9/Main.java
@@ -6,42 +6,29 @@
 //	resto da divisão.
 import java.util.Scanner;
 public class Main{
-    public static void main(String[] args){
+    public static void main(){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Escolha o que deseja fazer com os números:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Resto da divisão\nDigite o número da opção desejada: ");
-        int opcao = sc.nextInt();
-        System.out.print("Digite o primeiro número: ");
-        int num1 = sc.nextInt();
-        System.out.print("Digite o segundo número: ");
-        int num2 = sc.nextInt(); 
+        System.out.println("Digite o primeiro número:");
+        double num1 = sc.nextDouble();
 
-        switch(opcao){
-            case 1:
-                System.out.println("O resultado da soma é: " + (num1 + num2));
-                break;
-            case 2:
-                System.out.println("O resultado da subtração é: " + (num1 - num2));
-                break;
-            case 3:
-                System.out.println("O resultado da multiplicação é: " + (num1 * num2));
-                break;
-            case 4:
-                if(num2 != 0){
-                    System.out.println("O resultado da divisão é: " + (num1 / num2));
-                } else {
-                    System.out.println("Erro: Divisão por zero não é permitida.");
-                }
-                break;
-            case 5:
-                if(num2 != 0){
-                    System.out.println("O resultado do resto dessa divisão é: " + (num1 / num2));
-                } else {
-                    System.out.println("Erro: Divisão por zero não é permitida.");
-                }    
-                break;
-            default:
-                System.out.println("Opção inválida.");            
+        System.out.println("Digite o segundo número:");
+        double num2 = sc.nextDouble();
+
+        double soma = num1 + num2;
+        double subtracao = num1 - num2;
+        double multiplicacao = num1 * num2;
+        double divisao = num1 / num2;
+        double restoDivisao = num1 % num2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        if(num2 != 0){
+            System.out.println("Divisão: " + divisao);
+            System.out.println("Resto da divisão: " + restoDivisao);
+        } else {
+            System.out.println("Divisão por zero não é permitida.");
         }
     }
 }
