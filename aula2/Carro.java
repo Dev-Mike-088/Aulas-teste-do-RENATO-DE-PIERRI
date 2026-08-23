64a381898c3e768d13daff5645c6d87831cbbea7
Mikael <mikaeleal28@gmail.com>
2026-08-23 20:52:20 -0300

    Subindo aula2 do Prof.Renato

---
+++ aula2/Carro.java
@@ -0,0 +1,49 @@
+public class Carro{
+    String marca;
+    String modelo;
+    String cor;
+    int velocidade;
+    int ano;
+    double combustivel;
+    boolean ligado = false;
+
+    public boolean ligar(){
+        System.out.println("Ligando o Carro " + modelo);
+        return ligado = true;
+    }
+
+    public boolean desligar(){
+        System.out.println("Desligando o Carro " + modelo);
+        return ligado = false;
+    }
+
+    public void acelerar(){
+        if (ligado){
+            System.out.println("Acelerando.......");
+            velocidade = velocidade + 10;
+            combustivel = combustivel - 8.5;
+        } else{
+            System.out.println("ligue o carro antes de acelerar, por favor");
+        }
+    }
+    
+    public void frear(){
+        if (ligado){
+            System.out.println("Freando.......");
+            velocidade = velocidade - 10;
+        } else{
+            System.out.println("ligue o carro antes de executar qualquer açãop, por favor");
+        }
+    }
+
+    public void mostrarDados(){
+        System.out.println("======Dados do Veículo======");
+        System.out.println("Marca: " + marca );
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Cor: " + cor);
+        System.out.println("Ano: " + ano);
+        System.out.println("Combustivel: " + combustivel + "L");
+        System.out.println("Velocidade: " + velocidade);
+        System.out.println("Está ligado: " + ligado + "\n");        
+    }
+}
---
+++ aula2/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+        Carro ferrari = new Carro();
+        ferrari.marca = "Ferrari"; 
+        ferrari.modelo = "LaFerrari";
+        ferrari.cor = "Vermelho";
+        ferrari.velocidade = 0;
+        ferrari.combustivel = 46.0;
+
+        System.out.println ("Estado Inicial: ");
+        ferrari.mostrarDados();
+        System.out.println("Acelerando...");
+
+        ferrari.ligar();
+        
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.mostrarDados();
+        
+        System.out.println ("Freando ...");
+        ferrari.frear();
+        
+        ferrari.mostrarDados();
+
+        ferrari.desligar();
+
+        ferrari.mostrarDados();
+    }
+    
+}
