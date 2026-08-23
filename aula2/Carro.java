24fdf7eaa81fb8454255e41e4a70f45a513e3bf4
Mikael <mikaeleal28@gmail.com>
2026-08-23 20:58:49 -0300

    Alteração Simples no projeto

--- aula2/Carro.java
+++ aula2/Carro.java
@@ -23,7 +23,7 @@ public class Carro{
             velocidade = velocidade + 10;
             combustivel = combustivel - 8.5;
         } else{
-            System.out.println("ligue o carro antes de acelerar, por favor");
+            System.out.println("ligue o carro antes de executar qualquer ação, por favor");
         }
     }
     
@@ -32,7 +32,15 @@ public class Carro{
             System.out.println("Freando.......");
             velocidade = velocidade - 10;
         } else{
-            System.out.println("ligue o carro antes de executar qualquer açãop, por favor");
+            System.out.println("ligue o carro antes de executar qualquer ação, por favor");
+        }
+    }
+
+    public void buzinar(){
+        if (ligado){
+            System.out.println("Biiiiiiiiiipppppppppppp");
+        } else{
+            System.out.println("ligue o carro antes de executar qualquer ação, por favor");
         }
     }
 
